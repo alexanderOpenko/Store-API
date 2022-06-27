2f983d0d3bb4c9e36538c1d59599bef1f426920a
alexanderOpenko <openkooleksandr@gmail.com>
2022-06-27 12:50:04 +0300

    Apply func explode to refactor code

--- create_product.php
+++ create_product.php
@@ -1,11 +1,17 @@
 <?php
 header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+header('Access-Control-Allow-Credentials: true');
+header('Content-Type: application/json');
+
 require 'db_connect.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    //set product
-    $sql_product = "INSERT INTO Products (name)
-        VALUES ('$_POST[product_name]')";
+    $content = trim(file_get_contents("php://input"));
+    $decoded = json_decode($content, true);
+
+    $sql_product = "INSERT INTO Products (name, price, collection)
+        VALUES ('$decoded[product_name]', '$decoded[colletion]', '$decoded[prod_price]')";
 
     $conn->query($sql_product);
     $last_prod_id = $conn->insert_id;
@@ -13,20 +19,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $mods = [];
 
     //set variants
-    for ($i = 0; $i < count($_POST['modification']); $i++) {
-        $mods[$i] = array('variant_title' => $_POST['modification'][$i]);
-
-        if (count($_POST['price']) >= 2) {
-            $mods[$i]['price'] = $_POST['price'][$i];
-            $mods[$i]['qty'] = $_POST['qty'][$i];
+    for ($i = 0; $i < count($decoded['modification[]']); $i++) {
+        if (count($decoded['modification[]']) >= 2) {
+            $mods[$i] = array('variant_title' => $decoded['modification[]'][$i]);
+            $mods[$i]['price'] = $decoded['price'][$i];
+            $mods[$i]['qty'] = $decoded['qty'][$i];
         } else {
-            $mods[$i]['price'] = $_POST['price'];
-            $mods[$i]['qty'] = $_POST['qty'];
+            $mods[$i] = array('variant_title' => $decoded['modification[]']);
+            $mods[$i]['price'] = $decoded['price'];
+            $mods[$i]['qty'] = $decoded['qty'];
         }
-        foreach ($_POST['options-' . +($i + 1)] as $value) {
-            $mods[$i]['options'][] = $value;
-        }
+        $mods[$i]['options'] = $decoded['options'];
     }
+
+    //print json_encode($mods);
 
     foreach ($mods as $value) {
         $sql_variants = "INSERT INTO Modifications (mod_title, qty, price)
@@ -46,18 +52,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         $conn->query($sql_create_opt_row);
 
-        for ($i = 0; $i < count($value['options']); $i++) {
+        $options = explode(', ', $value['options']);
+        print json_encode($options);
+        for ($i = 0; $i < count($options); $i++) {
             $opt = 'opt' . +($i + 1);
-            $opt_value = $value['options'][$i];
+            $opt_value = $options[$i];
 
             $sql_fill_options = $conn->prepare("UPDATE Options SET $opt = ?");
 
             $sql_fill_options->bind_param('s', $opt_value);
 
-          $sql_fill_options->execute();
+            $sql_fill_options->execute();
         }
-
-        print_r($mods);
     }
 }
 ?>
+
+
