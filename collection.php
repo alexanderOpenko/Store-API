e1d8f60f3c37acaafe31f8285f0fc9565003c24a
alexanderOpenko <openkooleksandr@gmail.com>
2022-07-20 15:08:34 +0300

    Refactor collection to class

--- collection.php
+++ collection.php
@@ -1,97 +1,141 @@
 <?php
 header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Credentials: true');
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    require 'db_connect.php';
-    $collection = $_GET['collection'];
-    $products = [];
+function sendImageURL ($img_name) {
+    $base_dir = __DIR__;
+    $doc_root = preg_replace("!${_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']);
+    $base_url = preg_replace("!^${doc_root}!", '', $base_dir);
+    $protocol = empty($_SERVER['HTTPS']) ? 'http' : 'https';
+    $port = $_SERVER['SERVER_PORT'];
+    $disp_port = ($protocol == 'http' && $port == 80 || $protocol == 'https' && $port == 443) ? '' : ":$port";
+    $domain = $_SERVER['SERVER_NAME'];
+    $img_url = "${protocol}://${domain}${disp_port}${base_url}/assets/${img_name}";
 
-    function sendImageURL ($img_name) {
-        $base_dir = __DIR__;
-        $doc_root = preg_replace("!${_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']);
-        $base_url = preg_replace("!^${doc_root}!", '', $base_dir);
-        $protocol = empty($_SERVER['HTTPS']) ? 'http' : 'https';
-        $port = $_SERVER['SERVER_PORT'];
-        $disp_port = ($protocol == 'http' && $port == 80 || $protocol == 'https' && $port == 443) ? '' : ":$port";
-        $domain = $_SERVER['SERVER_NAME'];
-        $img_url = "${protocol}://${domain}${disp_port}${base_url}/assets/${img_name}";
-        return $img_url;
+    return $img_url;
+}
+
+class Product {
+    public $collection;
+    public $products = [];
+
+    public function __construct($collection){
+        $this->collection = $collection;
+        $this->start();
     }
 
-    $sql_prod = "SELECT id, name, main_photo, products.price, collection
-            FROM Products WHERE collection = '$collection'";
-
-    $result_prod = $conn->query($sql_prod);
-
-    if (!$result_prod) {
-        printf("Errormessage: %s\n", $conn->error);
+    public function start () {
+        $this->getProducts();
     }
 
-    $rows = $result_prod->fetch_all(MYSQLI_ASSOC);
+    public function getVariants ($key, $prod_id) {
+        require 'db_connect.php';
 
-    foreach ($rows as $row) {
-        $products[] = $row;
-    }
-
-    foreach ($products as $key => $value) {
-            $sql_mods = "SELECT mod_title, modifications.mod_id, qty, mod_photo, modifications.price,
+        $sql_mods = "SELECT mod_title, modifications.mod_id, qty, modifications.price,
                 opt1, opt2, opt3
                 FROM prod_mods JOIN modifications
-                ON prod_mods.mod_id = modifications.mod_id JOIN options 
-                ON options.mod_id = modifications.mod_id 
-                WHERE prod_id = $value[id]";
+                ON prod_mods.mod_id = modifications.mod_id JOIN options
+                ON options.mod_id = modifications.mod_id
+                WHERE prod_id = $prod_id";
 
-            $sql_params = "SELECT name FROM params WHERE prod_id = $value[id]";
+        $mods = $this->sql_fetch_all($sql_mods, MYSQLI_ASSOC);
 
-            $sql_product_photos = "SELECT img1, img2, img3, img4 FROM photos
-            WHERE prod_id = $value[id]";
+        if ($mods) {
+            //set images for variants
+            foreach($mods as $m_key => $m_value) {
+                $mod_id = $m_value[mod_id];
 
-            $result_mods = $conn->query($sql_mods);
-            $result_params = $conn->query($sql_params);
-            $result_photos = $conn->query($sql_product_photos);
+                $sql_mod_images = "SELECT img1, img2, img3, img4 FROM variant_images WHERE mod_id = $mod_id";
 
-            if ($result_mods) {
-                $rows_mod = $result_mods->fetch_all(MYSQLI_ASSOC);
+                $rows_mod_images = $this->sql_fetch_all($sql_mod_images, MYSQLI_NUM);
 
-                foreach($rows_mod as $m_key => $m_value) {
-                    $mod_id = $m_value[mod_id];
-
-                    $sql_mod_images = "SELECT img1, img2, img3, img4 FROM variant_images WHERE mod_id = $mod_id";
-                    $result_mod_images = $conn->query($sql_mod_images);
-                    $rows_mod_images = $result_mod_images->fetch_all(MYSQLI_ASSOC);
-
-                    $rows_mod[$m_key]['mod_images'] = $rows_mod_images;
-                }
-
-                for ($i = 0; $i < count($rows_mod); $i++) {
-                    $rows_mod[$i]['mod_photo'] = sendImageURL($rows_mod[0]['mod_photo']);
-                }
-
-                $products[$key]['modifications'] = $rows_mod;
-            }
-
-            if($result_params) {
-                $rows_params = $result_params->fetch_all(MYSQLI_ASSOC);
-
-                foreach($rows_params as $val) {
-                    $products[$key]['params'][] = $val[name];
-                }
-            }
-
-            if($result_photos) {
-                $photo_params = $result_photos->fetch_all(MYSQLI_NUM);
-
-                for ($i = 0; $i < count($photo_params[0]); $i++) {
-                    $img = $photo_params[0][$i];
+                for ($i = 0; $i < count($rows_mod_images[0]); $i++) {
+                    $img = $rows_mod_images[0][$i];
 
                     if ($img) {
                         $img_url = sendImageURL($img);
-                        $GLOBALS["products"][$key]['images'][] = $img_url;
+                        $mods[$m_key]['mod_images'][] = $img_url;
+                    }
+                }
+            }
+
+            $this->products[$key]['modifications'] = $mods;
+        }
+
+        return $mods;
+    }
+
+    public function getProducts () {
+        $opt1 = [];
+        $opt2 = [];
+        $opt3 = [];
+
+        $sql_prod = "SELECT id, name, main_photo, products.price, collection
+            FROM Products WHERE collection = '$this->collection'";
+
+        $this->products = $this->sql_fetch_all($sql_prod, MYSQLI_ASSOC);
+
+        foreach ($this->products as $key => $value) {
+            $this->products[$key]['main_photo'] = sendImageURL($this->products[$key]['main_photo']);
+
+            $mods = $this->getVariants($key, $value[id]);
+
+            foreach ($mods as $v) {
+
+                if (!in_array($v[opt1], $opt1)) {
+                    $opt1[] = $v[opt1];
+                }
+
+                if (!in_array($v[opt2], $opt2)) {
+                    $opt2[] = $v[opt2];
+                }
+
+                if (!in_array($v[opt3], $opt3)) {
+                    $opt3[] = $v[opt3];
+                }
+            }
+
+            $this->products[$key]['options'][] = $opt1;
+            $this->products[$key]['options'][] = $opt2;
+            $this->products[$key]['options'][] = $opt3;
+
+
+            $sql_params = "SELECT name FROM params WHERE prod_id = $value[id]";
+            $prod_params = $this->sql_fetch_all($sql_params, MYSQLI_ASSOC);
+
+            if ($prod_params) {
+                foreach ($prod_params as $val) {
+                    $this->products[$key]['params'][] = $val[name];
+                }
+            }
+
+            $sql_product_images = "SELECT img1, img2, img3, img4 FROM photos
+            WHERE prod_id = $value[id]";
+
+            $product_images = $this->sql_fetch_all($sql_product_images, MYSQLI_NUM);
+
+            if ($product_images) {
+                for ($i = 0; $i < count($product_images[0]); $i++) {
+                    $img = $product_images[0][$i];
+
+                    if ($img) {
+                        $img_url = sendImageURL($img);
+                        $this->products[$key]['images'][] = $img_url;
                     }
                 }
             }
         }
 
- print json_encode($products);
+        print json_encode($this->products);
+    }
+
+    public function sql_fetch_all ($sql_string, $fetch_type) {
+        require 'db_connect.php';
+        $rows = $conn->query($sql_string);
+
+        return $rows->fetch_all($fetch_type);
+    }
 }
+
+new Product($_GET['collection']);
 ?>
